0f0b5d2e7f75440eb9cbe6de5afe363f7faa86c1
2017010390 <57566782+2017010390@users.noreply.github.com>
2023-11-20 10:43:39 +0800

    add UI and modify game 1.add UI display

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let playerSelection = "rock";
 let computerSelection = getComputerChoice();
 
+// game score
+const winPoint = 5;
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
 /**
  * random computer choice
  * @returns string
@@ -21,26 +27,113 @@ function playRound(playerSelection, computerSelection)
 {
     playerSelection = playerSelection.toLowerCase();
     if(playerSelection == computerSelection){
-        console.log("Tie Game!");
+        roundWinner = 'tie';
     } else if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")
     ){
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        computerScore++;
+        roundWinner = 'computer';
     } else {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        roundWinner = 'player';
     }
 }
 
-function game()
+function isEndGame()
 {
-    for(let i = 1; i <= 5; ++i)
+    return playerScore === winPoint || computerScore === winPoint;
+}
+
+function game(event)
+{
+    playerSelection = event.target.id;
+    computerSelection = getComputerChoice();
+    updateChoice(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    updateBoard(roundWinner);
+    updateScore();
+}
+
+/**
+ * UI
+ */
+//get hint element
+const gameResult = document.querySelector('.game-result');
+const gameDetail = document.querySelector('.game-detail');
+
+const playerBoxScore = document.querySelector('.score.player');
+const playerBoxSign = document.querySelector('.role.player');
+const computerBoxScore = document.querySelector('.score.computer');
+const computerBoxSign = document.querySelector('.role.computer');
+
+//get btn element
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+/**
+ * btnRock Listener: click
+ */
+btnRock.addEventListener('click', game);
+
+/**
+ * btnPaper Listener: click
+ */
+btnPaper.addEventListener('click', game);
+
+/**
+ * btnScissors Listener: click
+ */
+btnScissors.addEventListener('click', game);
+
+function updateBoard(roundWinner)
+{
+    if(roundWinner == 'tie')
     {
-        playerSelection = prompt("Please input ROCK PAPER or SCISSORS") || "rock";
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        gameResult.textContent = 'Tie Game';
+        gameDetail.textContent = `${playerSelection} ties ${computerSelection}`;
+    }
+    else if(roundWinner == 'player')
+    {
+        gameResult.textContent = 'You Win!';
+        gameDetail.textContent = `${playerSelection} beats ${computerSelection}`;
+    }
+    else
+    {
+        gameResult.textContent = 'You Lose!';
+        gameDetail.textContent = `${playerSelection} beated ${computerSelection}`;
     }
 }
 
-game();
+function updateChoice(playerSelection, computerSelection)
+{
+    if(playerSelection === 'rock')
+    {
+        playerBoxSign.textContent = '✊';
+    } else if(playerSelection === 'paper')
+    {
+        playerBoxSign.textContent = '✋';
+    } else
+    {
+        playerBoxSign.textContent = '✌';
+    }
+    
+    if(computerSelection === 'rock')
+    {
+        computerBoxSign.textContent = '✊';
+    } else if(computerSelection === 'paper')
+    {
+        computerBoxSign.textContent = '✋';
+    } else
+    {
+        computerBoxSign.textContent = '✌';
+    }
+}
+
+function updateScore()
+{
+    playerBoxScore.textContent = `Player:${playerScore}`;
+    computerBoxScore.textContent = `Computer:${computerScore}`;
+}
